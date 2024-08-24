2712dddbb41bcd0757a74c2dc10e5057ccb528d4
JUANFPALECHOR <juanfelipepalechor@gmail.com>
2024-08-24 12:04:07 -0500

    parte de pruebas unitarias y documentacion

--- src/main/java/com/pragma/arquetipobootcamp2024/domain/spi/ICategoryRepository.java
+++ src/main/java/com/pragma/arquetipobootcamp2024/domain/spi/ICategoryRepository.java
@@ -4,7 +4,13 @@ import com.pragma.arquetipobootcamp2024.domain.model.Category;
 
 import java.util.Optional;
 
+
+
+//Class in which I tell him that he must maintain his category but without telling him how.
+
+
+
 public interface ICategoryRepository {
-    void save(Category category);
-    Optional<Category> findByName(String name);
+    void save(Category category); // Save a Category but returns nothing
+    Optional<Category> findByName(String name); //search a category by name
 }
