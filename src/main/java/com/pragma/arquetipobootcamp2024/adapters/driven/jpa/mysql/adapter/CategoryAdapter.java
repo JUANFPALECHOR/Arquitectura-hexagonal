f1ee9112323df69f26c1e8d85a91913c5690f1ac
JUANFPALECHOR <juanfelipepalechor@gmail.com>
2024-08-31 18:45:55 -0500

    documentacion de category adapter

--- src/main/java/com/pragma/arquetipobootcamp2024/adapters/driven/jpa/mysql/adapter/CategoryAdapter.java
+++ src/main/java/com/pragma/arquetipobootcamp2024/adapters/driven/jpa/mysql/adapter/CategoryAdapter.java
@@ -21,6 +21,15 @@ public class CategoryAdapter implements ICategoryRepository {
     private final CategoryRepository categoryRepository;
     private final ICategoryEntityMapper categoryEntityMapper;
 
+    /**
+     * Save a category to the database.
+     * First, convert the Category domain object to a JPA entity (CategoryEntity)
+     * using the mapper, then saves it to the database.
+     *
+     * @param category The category of the domain to save.
+     * @throws IllegalArgumentException If the category name is null.
+     */
+
     @Override
     public void save(Category category) {
         CategoryEntity categoryEntity = categoryEntityMapper.toEntity(category);
@@ -28,17 +37,24 @@ public class CategoryAdapter implements ICategoryRepository {
         if (categoryEntity.getName() == null) {
             throw new IllegalArgumentException("The name field must not be null");
         }
-        categoryRepository.save(categoryEntity);
+
+        categoryRepository.save(categoryEntity);// Save the entity to the database
     }
 
+
+    // Converts the obtained JPA entity to a Category domain object
     @Override
     public Optional<Category> findByName(String name) {
+        // Looks up the entity in the database and converts it to a domain object
         return categoryRepository.findByName(name)
                 .map(categoryEntityMapper::toDomain);
     }
 
+
+    // Returns a paginated list of categories.
     @Override
     public Page<Category> findAll(Pageable pageable) {
+        // Gets the paged entities and converts them to domain objects
         return categoryRepository.findAll(pageable)
                 .map(categoryEntityMapper::toDomain);  //
     }
