81727c4dbbc7e2b485e2e3982a64602dfd6ded62
JUANFPALECHOR <juanfelipepalechor@gmail.com>
2024-09-03 10:26:54 -0500

    I create markup model in the domain layer and the repository interface in which I tell it what I want to do

---
+++ src/main/java/com/pragma/arquetipobootcamp2024/domain/spi/IBrandRepository.java
@@ -0,0 +1,11 @@
+package com.pragma.arquetipobootcamp2024.domain.spi;
+
+
+import com.pragma.arquetipobootcamp2024.domain.model.Brand;
+import java.util.Optional;
+
+public interface IBrandRepository {
+    void save(Brand brand);
+    Optional<Brand> findByName(String name);
+}
+
