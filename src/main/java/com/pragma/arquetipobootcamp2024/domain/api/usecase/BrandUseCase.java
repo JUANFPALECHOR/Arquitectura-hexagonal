cc09092fae9ade64ee9c94d9ef2dd948b544c221
JUANFPALECHOR <juanfelipepalechor@gmail.com>
2024-09-04 14:31:07 -0500

    Implemente BrandUseCase

---
+++ src/main/java/com/pragma/arquetipobootcamp2024/domain/api/usecase/BrandUseCase.java
@@ -0,0 +1,40 @@
+package com.pragma.arquetipobootcamp2024.domain.api.usecase;
+
+import com.pragma.arquetipobootcamp2024.domain.model.Brand;
+import com.pragma.arquetipobootcamp2024.domain.spi.IBrandRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+import static com.pragma.arquetipobootcamp2024.domain.util.DomainConstants.*;
+
+@Service
+public class BrandUseCase {
+
+    private final IBrandRepository brandRepository;
+
+    public BrandUseCase(IBrandRepository brandRepository) {
+        this.brandRepository = brandRepository;
+    }
+
+    public void createBrand(Brand brand) {
+        validateBrand(brand);
+        Optional<Brand> existingBrand = brandRepository.findByName(brand.getName());
+        if (existingBrand.isPresent()) {
+            throw new IllegalArgumentException(ERROR_BRAND_NAME_EXISTS);
+        }
+        brandRepository.save(brand);
+    }
+
+    private void validateBrand(Brand brand) {
+        if (brand.getName().length() > 50) {
+            throw new IllegalArgumentException(ERROR_BRAND_NAME_MAX_LENGTH);
+        }
+        if (brand.getDescription().length() > 120) {
+            throw new IllegalArgumentException(ERROR_BRAND_DESCRIPTION_MAX_LENGTH);
+        }
+        if (brand.getName().isEmpty() || brand.getDescription().isEmpty()) {
+            throw new IllegalArgumentException(ERROR_BRAND_FIELDS_EMPTY);
+        }
+    }
+}
--- src/main/java/com/pragma/arquetipobootcamp2024/domain/util/DomainConstants.java
+++ src/main/java/com/pragma/arquetipobootcamp2024/domain/util/DomainConstants.java
@@ -22,4 +22,12 @@ public final class DomainConstants {
     public static final String ERROR_CATEGORY_NAME_MAX_LENGTH = "El tamaño máximo del nombre debe ser de 50 caracteres.";
     public static final String ERROR_CATEGORY_DESCRIPTION_MAX_LENGTH = "El tamaño máximo de la descripción debe ser de 90 caracteres.";
     public static final String ERROR_CATEGORY_FIELDS_EMPTY = "El nombre y la descripción no pueden estar vacíos.";
+
+    public static final String ERROR_BRAND_NAME_EXISTS = "El nombre de la marca ya existe.";
+    public static final String ERROR_BRAND_NAME_MAX_LENGTH = "El nombre de la marca debe ser menor o igual a 50 caracteres.";
+    public static final String ERROR_BRAND_DESCRIPTION_MAX_LENGTH = "La descripción de la marca debe ser menor o igual a 120 caracteres.";
+    public static final String ERROR_BRAND_FIELDS_EMPTY = "El nombre y la descripción de la marca no pueden estar vacíos.";
+    public static final String FIELD_CANNOT_BE_EMPTY = "El campo no puede estar vacío.";
+    public static final String FIELD_CANNOT_EXCEED_MAX_LENGTH = "El campo no puede exceder el número máximo de caracteres permitido.";
+
 }
