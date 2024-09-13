cc3b9144366cb9c34ca8e309f7d965b4ac30ec42
JUANFPALECHOR <juanfelipepalechor@gmail.com>
2024-09-13 17:17:55 -0500

    agg paginacion en adapter y dominio icategoryrepositoy

--- src/main/java/com/pragma/arquetipobootcamp2024/adapters/driven/jpa/mysql/adapter/BrandAdapter.java
+++ src/main/java/com/pragma/arquetipobootcamp2024/adapters/driven/jpa/mysql/adapter/BrandAdapter.java
@@ -1,11 +1,14 @@
 package com.pragma.arquetipobootcamp2024.adapters.driven.jpa.mysql.adapter;
 
 import com.pragma.arquetipobootcamp2024.domain.model.Brand;
+import com.pragma.arquetipobootcamp2024.domain.model.Category;
 import com.pragma.arquetipobootcamp2024.domain.spi.IBrandRepository;
 import com.pragma.arquetipobootcamp2024.adapters.driven.jpa.mysql.entity.BrandEntity;
 import com.pragma.arquetipobootcamp2024.adapters.driven.jpa.mysql.mapper.IBrandEntityMapper;
 import com.pragma.arquetipobootcamp2024.adapters.driven.jpa.mysql.repository.BrandRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -27,5 +30,12 @@ public class BrandAdapter implements IBrandRepository {
     public Optional<Brand> findByName(String name) {
         return brandRepository.findByName(name)
                 .map(brandEntityMapper::toDomain);
+    }
+
+    @Override
+    public Page<Brand> findAll(Pageable pageable) {
+        // Gets the paged entities and converts them to domain objects
+        return brandRepository.findAll(pageable)
+                .map(brandEntityMapper::toDomain);  //
     }
 }
--- src/main/java/com/pragma/arquetipobootcamp2024/domain/spi/IBrandRepository.java
+++ src/main/java/com/pragma/arquetipobootcamp2024/domain/spi/IBrandRepository.java
@@ -2,10 +2,14 @@ package com.pragma.arquetipobootcamp2024.domain.spi;
 
 
 import com.pragma.arquetipobootcamp2024.domain.model.Brand;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import java.util.Optional;
 
 public interface IBrandRepository {
     void save(Brand brand);
     Optional<Brand> findByName(String name);
+    Page<Brand> findAll(Pageable pageable);
 
 }
