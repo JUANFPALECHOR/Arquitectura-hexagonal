9349b7882decfe697582396e5e0fdc08ff858525
JUANFPALECHOR <juanfelipepalechor@gmail.com>
2024-09-18 10:44:50 -0500

    creacion de article en la capa de persistencia

---
+++ src/main/java/com/pragma/arquetipobootcamp2024/domain/model/Article.java
@@ -0,0 +1,14 @@
+package com.pragma.arquetipobootcamp2024.domain.model;
+
+import java.util.List;
+
+public class Article {
+
+    private Long id;
+    private String name;
+    private String description;
+    private int quantity;
+    private double price;
+    private List<Category> categories;
+
+}
