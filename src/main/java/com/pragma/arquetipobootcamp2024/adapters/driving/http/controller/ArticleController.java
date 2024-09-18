bc16172dc392a9ba8f1d0cf66920a5966a435284
JUANFPALECHOR <juanfelipepalechor@gmail.com>
2024-09-18 12:25:07 -0500

    implementacion del controller

--- src/main/java/com/pragma/arquetipobootcamp2024/adapters/driving/http/controller/ArticleController.java
+++ src/main/java/com/pragma/arquetipobootcamp2024/adapters/driving/http/controller/ArticleController.java
@@ -1,4 +1,32 @@
 package com.pragma.arquetipobootcamp2024.adapters.driving.http.controller;
 
+import com.pragma.arquetipobootcamp2024.adapters.driving.http.dto.request.ArticleRequest;
+import com.pragma.arquetipobootcamp2024.adapters.driving.http.dto.response.ArticleResponse;
+import com.pragma.arquetipobootcamp2024.domain.api.usecase.ArticleUseCase;
+import com.pragma.arquetipobootcamp2024.domain.model.Article;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/articles")
 public class ArticleController {
+
+    private final ArticleUseCase articleUseCase;
+    private final IArticleMapper articleMapper;
+
+    public ArticleController(ArticleUseCase articleUseCase, IArticleMapper articleMapper) {
+        this.articleUseCase = articleUseCase;
+        this.articleMapper = articleMapper;
+    }
+
+    @PostMapping
+    public ResponseEntity<ArticleResponse> createArticle(@RequestBody ArticleRequest articleRequest) {
+        Article article = articleMapper.toDomain(articleRequest);
+        Article savedArticle = articleUseCase.createArticle(article);
+        ArticleResponse response = articleMapper.toResponse(savedArticle);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
 }
